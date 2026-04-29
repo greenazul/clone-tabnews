35bd02fd2160774ce7fc111b04dbd0215187cba1
greenazul <felipealmach@gmail.com>
2026-04-29 14:26:18 +0000

    alteração da mensagem inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Vamos tomar um mate? 🧉 </h1>
+    return <h1>Vamos tomar um mate? 🧉  <a href="https://wa.me/5511943757329" target="_blank">Confirme aqui!</a></h1>
 }
 
 export default Home;
